df2067ddb5b4ce2619f2d4f2b51b8c07a26293e5
Akinboyede, Joseph <jakinboy@volvocars.com>
2022-12-18 22:27:11 +0100

    Ifelif statment lesson

---
+++ ifelif_statement.sh
@@ -0,0 +1,14 @@
+
+echo -e "Enter the name of your sisters: \c"
+read -r a
+
+if [ $a = "tolu" ]; then
+	echo "First born"
+
+elif [ $a = "tope" ]; then
+	echo "Second born"
+
+elif [ $a = "tosin" ]; then
+	echo "Last born"
+
+fi
