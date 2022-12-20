fd16e257eeb241ad7baade41500020bf9319c204
Akinboyede, Joseph <jakinboy@volvocars.com>
2022-12-20 23:41:49 +0100

    user input update add atithmetic operato

--- read_user_input.sh
+++ read_user_input.sh
@@ -4,9 +4,12 @@ echo "Enter Your name: "
 read name
 echo "Enter your age: "
 read age
+BREAKPOINT=16
 
-if [ $age >= 16 ] ; then
+if [ $age -ge $BREAKPOINT ] ; then
     echo "You are good to start shell scripting $name "
 else
     echo "You need to be 16+ to start shell scripting $name "
+    diff=`expr $BREAKPOINT - $age`
+    echo "$name kindly wait for $diff years to reapply. Thank you $name"
 fi
