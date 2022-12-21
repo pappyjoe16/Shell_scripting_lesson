957c8eb0465d03a36a6321d39b921082bf61cb25
Akinboyede, Joseph <jakinboy@volvocars.com>
2022-12-21 15:15:35 +0100

    Update

--- read_user_input.sh
+++ read_user_input.sh
@@ -6,10 +6,14 @@ echo "Enter your age: "
 read age
 BREAKPOINT=16
 
-if [ $age -ge $BREAKPOINT ] ; then
+if [ $age -ge $BREAKPOINT ]; then
     echo "You are good to start shell scripting $name "
 else
     echo "You need to be 16+ to start shell scripting $name "
-    diff=`expr $BREAKPOINT - $age`
-    echo "$name kindly wait for $diff years to reapply. Thank you $name"
+    diff=$(expr $BREAKPOINT - $age)
+    if [ $diff -gt 1 ]; then
+        echo "$name kindly wait for $diff years to reapply. Thank you $name"
+    else
+        echo "$name kindly wait for $diff year to reapply. Thank you $name"
+    fi
 fi
