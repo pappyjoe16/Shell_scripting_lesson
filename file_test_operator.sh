b1211479e64fb9e57956b1c252abf695d8de7e99
Akinboyede Joseph <jakinboy@volvocars.com>
2023-01-07 01:10:38 +0100

    Add learnng about different flags

--- file_test_operator.sh
+++ file_test_operator.sh
@@ -4,7 +4,7 @@ userpass=(Joseph Nifemi)
 
 test_argument="read_user_input.sh"
 if [ -f $test_argument ]; then
-    echo "find file "
+    echo "found file "
     ./$test_argument "${userpass[@]}"
 else
     echo "file not found"
