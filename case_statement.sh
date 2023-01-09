a93613c92b1076cef910d948a977cf3c8b1f0a1d
Akinboyede Joseph <jakinboy@volvocars.com>
2023-01-09 02:31:30 +0100

    Alternative way of entering value on the same line as echo output

--- case_statement.sh
+++ case_statement.sh
@@ -2,19 +2,20 @@
 
 # Alternative way of entering value on the same line as echo output
 echo -e "Enter the type of transportation you will like to rent: \c"
+# or echo -e "Enter the type of transportation you will like to rent: " \\c
 read -r vehicle
 
 echo "You have entered $vehicle"
 sleep 0.5
-echo -ne "waiting for your bill." \\r
+echo -ne "waiting for your bill. \r"
 sleep 0.5
-echo -ne "waiting for your bill.." \\r
+echo -ne "waiting for your bill.. \r"
 sleep 0.5
-echo -ne "waiting for your bill..." \\r
+echo -ne "waiting for your bill... \r"
 sleep 0.5
-echo -ne "waiting for your bill...." \\r
+echo -ne "waiting for your bill.... \r"
 sleep 0.5
-echo -ne "                               "\\r
+echo -ne "                               \r"
 case $vehicle in
 "Car")
     echo "Rent of $vehicle is 400 dollars"
