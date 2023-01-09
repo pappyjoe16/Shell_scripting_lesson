8825a383a8b4de4c49331e4c2cfc553e49071228
Akinboyede Joseph <jakinboy@volvocars.com>
2023-01-09 01:56:45 +0100

    User entered test case to check for

--- case_statement.sh
+++ case_statement.sh
@@ -1,27 +1,34 @@
 #! /bin/bash
 
-vehicle=$3
+# Alternative way of entering value on the same line as echo output
+echo -e "Enter the type of transportation you will like to rent: \c"
+read -r vehicle
 
 echo "You have entered $vehicle"
-sleep 1
+sleep 0.5
 echo -ne "waiting for your bill." \\r
-sleep 1
+sleep 0.5
 echo -ne "waiting for your bill.." \\r
-sleep 1
+sleep 0.5
 echo -ne "waiting for your bill..." \\r
-sleep 1
+sleep 0.5
 echo -ne "waiting for your bill...." \\r
-sleep 1
-echo -ne "waiting for your bill....." \\r
-sleep 1
+sleep 0.5
+echo -ne "                               "\\r
 case $vehicle in
-	"car" )
-		echo "Rent of $vehicle is 100 dollar" ;;
-	"van" )
-		echo "Rent of $vehicle is 200 dollar" ;;
-	"jeep" )
-		echo "Rent of $vehicle is 300 dollar" ;;
-	"bike" )
-		echo "Rent of $vehicle is 500 dollar" ;;
-
+"Car")
+    echo "Rent of $vehicle is 400 dollars"
+    ;;
+"Van")
+    echo "Rent of $vehicle is 200 dollars"
+    ;;
+"Truck")
+    echo "Rent of $vehicle is 300 dollars"
+    ;;
+"Bicycle")
+    echo "Rent of $vehicle is 50 dollars"
+    ;;
+*) # default case
+    echo "Unknown Vehicle"
+    ;;
 esac
