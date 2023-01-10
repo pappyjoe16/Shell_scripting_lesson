0cc11e1ec83284cddc8169830ce92736dc2e6949
Akinboyede Joseph <jakinboy@volvocars.com>
2023-01-10 02:52:59 +0100

    Add for loop with file and to execute commands

--- for_loop.sh
+++ for_loop.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
-
-for i in {0..10..2} ; do # {START..END..INCREMENT}
+for i in {0..10..2}; do # {START..END..INCREMENT}
     echo "$i"
-
 done
 
-for((i=0; i<=5; i++)) ; do # {init; condition; INCREMENT}
+for ((i = 0; i <= 5; i++)); do # {init; condition; INCREMENT}
     echo "$i"
+done
 
+# for loop with file and to execute commands
+for command in ls pwd date; do
+    echo "---------------$command--------------"
+    "$command"
+done
+
+for item in *; do # This will iterate through the present
+#working directory to chech the files inside either a file (-f) or directory (-d)
+    if [ -f "$item" ]; then
+        echo "$item"
+    fi
 done
