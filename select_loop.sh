ddff48d7df5244567b80be882e0b5036b8078a3d
Akinboyede Joseph <jakinboy@volvocars.com>
2023-01-14 22:38:44 +0100

    Add break and continue statement

--- select_loop.sh
+++ select_loop.sh
@@ -22,3 +22,11 @@ select food in Rice Poundo Spag Beans; do
     esac
     break
 done
+
+for ((i = 0; i <= 10; i++)); do
+    if [ "$i" -gt 5 ]; then
+        continue
+    fi
+    echo "$i"
+
+done
