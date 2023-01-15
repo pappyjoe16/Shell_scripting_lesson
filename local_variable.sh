dbb51c208ece1f5a83e7b024acfd9477e58cf0c9
Akinboyede Joseph <jakinboy@volvocars.com>
2023-01-15 02:25:21 +0100

    Add How to use && and || without if statement

--- local_variable.sh
+++ local_variable.sh
@@ -1,13 +1,30 @@
 #! /bin/bash
 
-function_local(){
+function_local() {
     local name="$1"
     echo "Welcome to you first local variable test $name"
 }
 
-name="NIfemi"
-echo "Welcome to you first local variable test $name"
+usage() {
+    echo "You need to provide an argument :"
+    echo "usage : $0 file_name"
+}
+
+is_file_exist() {
+    local file="$1" # $1 here is the argument provide to the function
+    [[ -f "$file" ]] && return 0 || return 1 # here if the file does not exist
+# it will return 0 and jump to "return 0"  but if file exist it will
+# return 1 and jump to "return 1"
+}
 
 function_local Joseph
 
-echo "Welcome to you first local variable test $name"
+[[ $# -eq 0 ]] && usage # here if there is no argument passed into the script
+# it will return 0 and jump to usage function but if argument is passed it will
+# return 1 and jump usage
+
+if ( is_file_exist "$1" ); then # $1 here is the argument provide to the script
+    echo "file found"
+else
+    echo "file not found"
+fi
