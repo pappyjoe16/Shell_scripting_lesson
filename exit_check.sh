0c9aee850b04d30620f1338866b23645fef4a2ee
Akinboyede Joseph <jakinboy@volvocars.com>
2023-02-02 15:33:25 +0100

    Exit code checker updated

--- exit_check.sh
+++ exit_check.sh
@@ -7,7 +7,8 @@ debug_hook_exit_code="$?"
 if [ "${debug_hook_exit_code}" -eq 0 ]; then
     echo "Debug hook success. Continuing with csp_startup.sh"
 elif [ "${debug_hook_exit_code}" -eq 245 ]; then
-    echo "Debug hook success, but got code 245. Will not continue with csp_startup.sh" && exit 245
+    echo "Debug hook success, but got code 245. Will not continue with csp_startup.sh"
+    exit 0
 else
     echo "Debug hook failed with code ${debug_hook_exit_code}. Continuing with csp_startup.sh"
 fi
