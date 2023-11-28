5758401292027d05e0d7d4a6824bc212fd553e02
Akinboyede Joseph <jakinboy@volvocars.com>
2023-11-28 13:27:23 +0100

    Error function to print in colour

---
+++ error_function.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+fail_with_error() {
+    local MSG="${1}"
+
+    local ENDCOLOR="\e[0m"
+    local RED="${ENDCOLOR}\e[31m"
+    local BOLDRED="${ENDCOLOR}\e[1;31m"
+
+    echo "
+${BOLDRED}####### Fatal error, aborting bitbake build ######
+
+${RED}$MSG
+
+${BOLDRED}##################################################${ENDCOLOR}
+    "
+}
+
+fail_with_error "THis is the error message"
