cb94017967653e19a4bb4d6b2c29c098a8868b83
Akinboyede Joseph <jakinboy@volvocars.com>
2024-02-01 14:20:49 +0100

    error function commit

--- error_function.sh
+++ error_function.sh
@@ -4,13 +4,13 @@ fail_with_error() {
     local MSG="${1}"
 
     local ENDCOLOR="\e[0m"
-    local RED="${ENDCOLOR}\e[31m"
+    local YELLOW="${ENDCOLOR}\e[31m"
     local BOLDRED="${ENDCOLOR}\e[1;31m"
 
     echo -e "
 ${BOLDRED}####### Fatal error, aborting bitbake build ######
 
-${RED}$MSG
+${YELLOW}$MSG
 
 ${BOLDRED}##################################################${ENDCOLOR}
     "
